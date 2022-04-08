d045a8a9dab316db5dedc5cc44f16c479f032620
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-08 09:13:17 +0300

    Create Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `
+  //   <h1>{{name}}</h1>
+  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'Pipes';
+  public name = 'Group 4';
+  words = 'I am a number of words. For the purpose of counting words using wordcount pipe. Adding more words'
+  number = 15;
+  public date = new Date();
 }
